5beafc8ee4f543f9d85fcacd925cdfe02e715718
rozbrajaczpoziomow <roz@rozbrajacz.futbol>
2025-02-27 12:42:07 +0100

    Fix textures and rendering, add translations for glass

---
+++ src/main/java/al132/alchemistry/blocks/GlassBlock.kt
@@ -0,0 +1,22 @@
+package al132.alchemistry.blocks
+
+import net.minecraft.block.SoundType
+import net.minecraft.block.state.IBlockState
+import net.minecraft.util.BlockRenderLayer
+import net.minecraft.util.EnumFacing
+import net.minecraft.util.math.BlockPos
+import net.minecraft.world.IBlockAccess
+
+class GlassBlock(name: String) : BaseBlock(name){
+    init {
+        this.soundType = SoundType.GLASS
+    }
+
+    override fun isOpaqueCube(state: IBlockState) = false
+
+    override fun shouldSideBeRendered(state: IBlockState, access: IBlockAccess, pos: BlockPos, side: EnumFacing): Boolean {
+        return access.getBlockState(pos.add(side.directionVec)).block != this
+    }
+
+    override fun getRenderLayer() = BlockRenderLayer.TRANSLUCENT
+}

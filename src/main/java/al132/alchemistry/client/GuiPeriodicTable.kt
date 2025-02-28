48d26981a0237be6b17410d835327ad2d1e38078
rozbrajaczpoziomow <roz@rozbrajacz.futbol>
2025-02-28 11:40:42 +0100

    Periodic Table - fixes pt. 1

--- src/main/java/al132/alchemistry/client/GuiPeriodicTable.kt
+++ src/main/java/al132/alchemistry/client/GuiPeriodicTable.kt
@@ -37,10 +37,10 @@ class GuiPeriodicTable : GuiScreen() {
         drawDefaultBackground()
 
         val scaledRes = ScaledResolution(Minecraft.getMinecraft())
-        val w = scaledRes.scaledWidth.coerceAtMost(2000)
-        val w_scale = w / 2000.0
-        val h = scaledRes.scaledHeight.coerceAtMost(1016)
-        val h_scale = h / 1016.0
+        val w = scaledRes.scaledWidth.coerceAtMost(1512)
+        val w_scale = w / 1512f
+        val h = scaledRes.scaledHeight.coerceAtMost(792)
+        val h_scale = h / 792f
 
         GlStateManager.color(1.0f, 1.0f, 1.0f, 1.0f)
         Minecraft.getMinecraft().textureManager.bindTexture(
@@ -52,64 +52,79 @@ class GuiPeriodicTable : GuiScreen() {
         drawScaledCustomSizeModalRect(0, 0, 0f, 0f, w, h, w, h, w.toFloat(), h.toFloat())
         drawCenteredString(fontRenderer, "alchemistry.gui.periodic_table".translate(), width / 2, 24, 0xFFFFFF)
 
-        val boxWidth = 27.75f * w_scale
-        val boxHeight = 26.9f * h_scale
-        val startX = ((width - (boxWidth * 18)) / 2) * w_scale
-        val startY = (((height - (boxHeight * 7)) / 2) - 33.0f) * h_scale
-        var count = 0
+        val boxWidth = 84f * w_scale
+        val boxHeight = 84f * h_scale
+        val group = (mouseX / boxWidth).toInt() + 1
+        val period = (mouseY / boxHeight).toInt() + 1
+        // TODO: account for La/Actinides (Hf/... gets replaced by them, + the Y gap in-between)
 
-        ElementRegistry.getAllElements().forEach { element ->
-            var x = startX
-            var y = startY
-            val group = element.group
-            val period = element.period
+        println("$group, $period")
+        val element = ElementRegistry.getAllElements().find { it.group == group && it.period == period }
 
-            if (group == 0 || period == 0) {
-                return@forEach
-            }
+        if(element != null)
+            drawElementTip(element)
 
-            for (row in 1 until 8) {
-                if (period == row) {
-                    for (col in 0 until 19) {
-                        if (group == col) {
-                            if (!((period == 6 || period == 7) && group == 3)) {
-                                if (mouseX >= x && mouseX <= x + boxWidth && mouseY >= y && mouseY <= y + boxHeight) {
-                                    drawElementTip(element)
-                                }
-                            } else {
-                                val resetX = x
-                                val resetY = y
-                                if (period == 6) {
-                                    y = (boxHeight * 7.45f) + startY
-                                    x = (boxWidth * count) + startX + boxWidth * 2
-                                    if (mouseX >= x && mouseX <= x + boxWidth && mouseY >= y && mouseY <= y + boxHeight) {
-                                        drawElementTip(element)
-                                    }
-                                    count++
-                                }
-                                if (period == 7) {
-                                    y = (boxHeight * 8.45f) + startY
-                                    x = (boxWidth * count - 15) + startX + boxWidth * 2
-                                    if (mouseX >= x && mouseX <= x + boxWidth && mouseY >= y && mouseY <= y + boxHeight) {
-                                        drawElementTip(element)
-                                    }
-                                    count++
-                                }
-                                y = resetY
-                                x = resetX
-                            }
-                        }
-                        x += boxWidth
-                    }
-                }
-                x = startX
-                y += boxHeight
-            }
-        }
+//        val startX = ((width - (boxWidth * 18)) / 2) * w_scale
+//        val startY = (((height - (boxHeight * 7)) / 2) - 33.0f) * h_scale
+//        println("scale: ${w_scale}, ${h_scale}; boxSize: ${boxWidth}x${boxHeight}; startXY: $startX, $startY")
+//        var count = 0
+
+//        println("$mouseX, $mouseY")
+//        println("$row, $col")
+        
+//        ElementRegistry.getAllElements().forEach { element ->
+//            var x = startX
+//            var y = startY
+//            val group = element.group
+//            val period = element.period
+//
+//            if (group == 0 || period == 0) {
+//                return@forEach
+//            }
+//
+//            for (row in 1 until 8) {
+//                if (period == row) {
+//                    for (col in 0 until 19) {
+//                        if (group == col) {
+//                            if (!((period == 6 || period == 7) && group == 3)) {
+//                                if (mouseX >= x && mouseX <= x + boxWidth && mouseY >= y && mouseY <= y + boxHeight) {
+//                                    drawElementTip(element)
+//                                }
+//                            } else {
+//                                val resetX = x
+//                                val resetY = y
+//                                if (period == 6) {
+//                                    y = (boxHeight * 7.45f) + startY
+//                                    x = (boxWidth * count) + startX + boxWidth * 2
+//                                    if (mouseX >= x && mouseX <= x + boxWidth && mouseY >= y && mouseY <= y + boxHeight) {
+//                                        drawElementTip(element)
+//                                    }
+//                                    count++
+//                                }
+//                                if (period == 7) {
+//                                    y = (boxHeight * 8.45f) + startY
+//                                    x = (boxWidth * count - 15) + startX + boxWidth * 2
+//                                    if (mouseX >= x && mouseX <= x + boxWidth && mouseY >= y && mouseY <= y + boxHeight) {
+//                                        drawElementTip(element)
+//                                    }
+//                                    count++
+//                                }
+//                                y = resetY
+//                                x = resetX
+//                            }
+//                        }
+//                        x += boxWidth
+//                    }
+//                }
+//                x = startX
+//                y += boxHeight
+//            }
+//        }
         GlStateManager.popMatrix()
         super.drawScreen(mouseX, mouseY, partialTicks)
     }
 
+    // TODO: fix scaling and overall drawing position
     private fun drawElementTip(element: ChemicalElement) {
         val scaledRes = ScaledResolution(Minecraft.getMinecraft())
         val w = scaledRes.scaledWidth.coerceAtMost(816)
@@ -119,7 +134,7 @@ class GuiPeriodicTable : GuiScreen() {
         Minecraft.getMinecraft().textureManager.bindTexture(
             ResourceLocation(
                 Reference.MODID,
-                String.format("textures/gui/elements/%s_tooltip.png", element.name)
+                "textures/gui/elements/${element.name}_tooltip.png"
             )
         )
         drawScaledCustomSizeModalRect(((this.width - 276) / 2) - 55, ((this.height - (7 * 28)) / 2) - 30, 0f, 0f, w, h, w, h, w.toFloat(), h.toFloat())

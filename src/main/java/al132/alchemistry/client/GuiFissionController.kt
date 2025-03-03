bb9f8ee2e05aa38942ca4cad441a2bb814d2666a
MasterEnderman <xxmr.endermanxx@gmail.com>
2025-03-03 13:25:30 +0100

    abstracted reactor gui

--- src/main/java/al132/alchemistry/client/GuiFissionController.kt
+++ src/main/java/al132/alchemistry/client/GuiFissionController.kt
@@ -2,9 +2,6 @@ package al132.alchemistry.client
 
 import al132.alchemistry.ConfigHandler
 import al132.alchemistry.tiles.TileFissionController
-import al132.alib.client.CapabilityEnergyDisplayWrapper
-import al132.alib.utils.Translator
-import net.minecraft.client.resources.I18n
 import net.minecraft.entity.player.InventoryPlayer
 import net.minecraft.util.ResourceLocation
 import java.awt.Color
@@ -13,19 +10,10 @@ import java.awt.Color
  * Created by al132 on 1/16/2017.
  */
 class GuiFissionController(playerInv: InventoryPlayer, tile: TileFissionController) :
-        GuiBase<TileFissionController>(ContainerFissionController(playerInv, tile), tile, GuiFissionController.textureLocation) {
-
+        GuiReactorController<TileFissionController>(ContainerFissionController(playerInv, tile), tile, textureLocation) {
 
     companion object {
         val textureLocation = ResourceLocation(root + "fission_controller_gui.png")
-    }
-
-    var statusText: String = ""
-
-    override val displayName = Translator.translateToLocal("tile.fission_controller.name")
-
-    init {
-        this.displayData.add(CapabilityEnergyDisplayWrapper(7, 10, 16, 60, tile::energyStorage))
     }
 
     override fun drawGuiContainerBackgroundLayer(partialTicks: Float, mouseX: Int, mouseY: Int) {
@@ -39,17 +27,8 @@ class GuiFissionController(playerInv: InventoryPlayer, tile: TileFissionControll
         }
     }
 
-    fun updateStatus() {
-        if (tile.isMultiblockValid) statusText = ""
-        else statusText = Translator.translateToLocal("tile.fission.invalid_multiblock")
-    }
-
     override fun drawGuiContainerForegroundLayer(mouseX: Int, mouseY: Int) {
         super.drawGuiContainerForegroundLayer(mouseX, mouseY)
-        updateStatus()
         this.fontRenderer.drawStringWithShadow(statusText, 30.0f, 100.0f, Color.WHITE.rgb)
-        // upgrades
-        this.fontRenderer.drawStringWithShadow(I18n.format("tile.fission.productivity", "%.2f%%".format(tile.productivityModifier * 100)), 8.0f, 120.0f, Color.WHITE.rgb)
-        this.fontRenderer.drawStringWithShadow(I18n.format("tile.fission.speed", "%.2f%%".format(tile.speedModifier * 100)), 8.0f, 130.0f, Color.WHITE.rgb)
     }
 }
---
+++ src/main/java/al132/alchemistry/client/GuiReactorController.kt
@@ -0,0 +1,77 @@
+package al132.alchemistry.client
+
+import al132.alchemistry.tiles.AbstractReactorController
+import al132.alchemistry.tiles.ReactorType
+import al132.alib.client.CapabilityEnergyDisplayWrapper
+import al132.alib.tiles.IGuiTile
+import al132.alib.utils.Translator
+import net.minecraft.client.resources.I18n
+import net.minecraft.inventory.Container
+import net.minecraft.util.ResourceLocation
+import java.awt.Color
+
+abstract class GuiReactorController<T>(container: Container, tile: T, textureLocation: ResourceLocation) :
+    GuiBase<T>(container, tile, textureLocation) where T : AbstractReactorController, T : IGuiTile {
+    override var displayName: String = ""
+    var textProductivity: String? = null
+    var textSpeed: String? = null
+    var textInvalid: String? = null
+
+    var statusText: String = ""
+
+    init {
+        this.displayData.add(CapabilityEnergyDisplayWrapper(7, 10, 16, 60, tile::energyStorage))
+        when (tile.reactorType) {
+            ReactorType.FISSION -> {
+                displayName = Translator.translateToLocal("tile.fission_controller.name")
+                textProductivity = "tile.fission.productivity"
+                textSpeed = "tile.fission.speed"
+                textInvalid = "tile.fission.invalid_multiblock"
+            }
+            ReactorType.FUSION -> {
+                displayName = Translator.translateToLocal("tile.fusion_controller.name")
+                textProductivity = "tile.fusion.productivity"
+                textSpeed = "tile.fusion.speed"
+                textInvalid = "tile.fusion.invalid_multiblock"
+            }
+        }
+    }
+
+    override fun drawGuiContainerForegroundLayer(mouseX: Int, mouseY: Int) {
+        super.drawGuiContainerForegroundLayer(mouseX, mouseY)
+        val productivity = tile.productivityModifier * 100
+        val speed = tile.speedModifier * 100
+        this.fontRenderer.drawString(
+            I18n.format("tile.fission.productivity", "%.2f%%".format(productivity)),
+            8.0f,
+            115.0f,
+            getColorFromValue(productivity),
+            false
+        )
+        this.fontRenderer.drawString(
+            I18n.format("tile.fission.speed", "%.2f%%".format(speed)),
+            8.0f,
+            125.0f,
+            getColorFromValue(speed),
+            false
+        )
+        updateStatus()
+    }
+
+    private fun updateStatus() {
+        statusText = if (tile.isMultiblockValid) {
+            ""
+        } else {
+            Translator.translateToLocal(textInvalid.toString())
+        }
+    }
+
+    private fun getColorFromValue(value: Double): Int {
+        return when {
+            value < 0 -> Color(148, 26, 26).rgb
+            value > 0 -> Color(27, 105, 27).rgb
+            else -> Color.GRAY.rgb
+        }
+    }
+
+}

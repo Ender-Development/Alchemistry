f7b8d010f5cba92408f4ad140dbafaa24620e784
MasterEnderman <xxmr.endermanxx@gmail.com>
2025-03-06 00:07:25 +0100

    consts and formatting

--- src/main/java/al132/alchemistry/client/GuiHandler.kt
+++ src/main/java/al132/alchemistry/client/GuiHandler.kt
@@ -10,42 +10,49 @@ import net.minecraftforge.fml.common.network.IGuiHandler
 class GuiHandler : IGuiHandler {
 
     companion object {
-        val NO_GUI = -1
-        val ELECTROLYZER_ID = 0
-        val CHEMICAL_DISSOLVER_ID = 1
-        val CHEMICAL_COMBINER_ID = 2
-        val EVAPORATOR_ID = 3
-        val ATOMIZER_ID = 4
-        val LIQUIFIER_ID = 5
-        val FISSION_CONTROLLER_ID = 6
-        val FUSION_CONTROLLER_ID = 7
+        const val NO_GUI = -1
+        const val ELECTROLYZER_ID = 0
+        const val CHEMICAL_DISSOLVER_ID = 1
+        const val CHEMICAL_COMBINER_ID = 2
+        const val EVAPORATOR_ID = 3
+        const val ATOMIZER_ID = 4
+        const val LIQUIFIER_ID = 5
+        const val FISSION_CONTROLLER_ID = 6
+        const val FUSION_CONTROLLER_ID = 7
     }
 
     override fun getServerGuiElement(ID: Int, player: EntityPlayer, world: World, x: Int, y: Int, z: Int): Any? {
         val entity = world.getTileEntity(BlockPos(x, y, z))
         when (ID) {
-            ELECTROLYZER_ID       -> {
+            ELECTROLYZER_ID -> {
                 if (entity is TileElectrolyzer) return ContainerElectrolyzer(player.inventory, entity)
             }
+
             CHEMICAL_DISSOLVER_ID -> {
                 if (entity is TileChemicalDissolver) return ContainerChemicalDissolver(player.inventory, entity)
             }
-            CHEMICAL_COMBINER_ID  -> {
+
+            CHEMICAL_COMBINER_ID -> {
                 if (entity is TileChemicalCombiner) return ContainerChemicalCombiner(player.inventory, entity)
             }
-            EVAPORATOR_ID         -> {
+
+            EVAPORATOR_ID -> {
                 if (entity is TileEvaporator) return ContainerEvaporator(player.inventory, entity)
             }
-            ATOMIZER_ID           -> {
+
+            ATOMIZER_ID -> {
                 if (entity is TileAtomizer) return ContainerAtomizer(player.inventory, entity)
             }
-            LIQUIFIER_ID          -> {
+
+            LIQUIFIER_ID -> {
                 if (entity is TileLiquifier) return ContainerLiquifier(player.inventory, entity)
             }
-            FISSION_CONTROLLER_ID          -> {
+
+            FISSION_CONTROLLER_ID -> {
                 if (entity is TileFissionController) return ContainerFissionController(player.inventory, entity)
             }
-            FUSION_CONTROLLER_ID          -> {
+
+            FUSION_CONTROLLER_ID -> {
                 if (entity is TileFusionController) return ContainerFusionController(player.inventory, entity)
             }
         }
@@ -55,28 +62,35 @@ class GuiHandler : IGuiHandler {
     override fun getClientGuiElement(ID: Int, player: EntityPlayer, world: World, x: Int, y: Int, z: Int): Any? {
         val entity = world.getTileEntity(BlockPos(x, y, z))
         when (ID) {
-            ELECTROLYZER_ID       -> {
+            ELECTROLYZER_ID -> {
                 if (entity is TileElectrolyzer) return GuiElectrolyzer(player.inventory, entity)
             }
+
             CHEMICAL_DISSOLVER_ID -> {
                 if (entity is TileChemicalDissolver) return GuiChemicalDissolver(player.inventory, entity)
             }
-            CHEMICAL_COMBINER_ID  -> {
+
+            CHEMICAL_COMBINER_ID -> {
                 if (entity is TileChemicalCombiner) return GuiChemicalCombiner(player.inventory, entity)
             }
-            EVAPORATOR_ID         -> {
+
+            EVAPORATOR_ID -> {
                 if (entity is TileEvaporator) return GuiEvaporator(player.inventory, entity)
             }
-            ATOMIZER_ID           -> {
+
+            ATOMIZER_ID -> {
                 if (entity is TileAtomizer) return GuiAtomizer(player.inventory, entity)
             }
-            LIQUIFIER_ID          -> {
+
+            LIQUIFIER_ID -> {
                 if (entity is TileLiquifier) return GuiLiquifier(player.inventory, entity)
             }
-            FISSION_CONTROLLER_ID          -> {
+
+            FISSION_CONTROLLER_ID -> {
                 if (entity is TileFissionController) return GuiFissionController(player.inventory, entity)
             }
-            FUSION_CONTROLLER_ID          -> {
+
+            FUSION_CONTROLLER_ID -> {
                 if (entity is TileFusionController) return GuiFusionController(player.inventory, entity)
             }
         }

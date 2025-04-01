3ce183c76251d050919ae3b491664fbb93aabecd
rozbrajaczpoziomow <roz@rozbrajacz.futbol>
2025-04-01 19:33:08 +0200

    meow at enderman a.k.a. implement what he left me as a TODO() earlier

--- src/main/java/io/enderdev/alchemistry/compat/groovyscript/content/Content.kt
+++ src/main/java/io/enderdev/alchemistry/compat/groovyscript/content/Content.kt
@@ -1,17 +1,19 @@
 package io.enderdev.alchemistry.compat.groovyscript.content
 
+import com.cleanroommc.groovyscript.api.GroovyBlacklist
 import com.cleanroommc.groovyscript.api.INamed
 import com.cleanroommc.groovyscript.api.documentation.annotations.Example
 import com.cleanroommc.groovyscript.api.documentation.annotations.MethodDescription
 import com.cleanroommc.groovyscript.api.documentation.annotations.RegistryDescription
 import com.cleanroommc.groovyscript.sandbox.GroovyLogImpl
-import io.enderdev.alchemistry.proxy.CommonProxy
 import io.enderdev.alchemistry.Tags
 import io.enderdev.alchemistry.chemistry.CompoundPair
 import io.enderdev.alchemistry.chemistry.CompoundRegistry
 import io.enderdev.alchemistry.chemistry.ElementRegistry
+import io.enderdev.alchemistry.proxy.CommonProxy
 import java.awt.Color
 import java.util.*
+import kotlin.math.roundToInt
 
 @RegistryDescription(linkGenerator = Tags.MOD_ID)
 class Content : INamed {
@@ -36,21 +38,18 @@ class Content : INamed {
 		}
 	}
 
-	@MethodDescription(
-		type = MethodDescription.Type.ADDITION,
-		example = [Example("200, 'Advanced Hydrogen Molecule', 205, 205, 205, [['advanced_hydrogen', 1],['advanced_hydrogen', 1],['advanced_hydrogen', 1]]", commented = true)]
-	)
-	fun createCompound(meta: Int, name: String, red: Int, green: Int, blue: Int, components: ArrayList<ArrayList<Any?>>) {
+	@GroovyBlacklist
+	fun createCompoundInternal(meta: Int, name: String, color: Color?, components: ArrayList<ArrayList<Any?>>) {
 		if(CommonProxy.getStage() != CommonProxy.LoadingStage.PRE_INIT) {
 			GroovyLogImpl.LOG.info("Compound creation is only allowed during preInit stage.")
 			return
 		}
-		if(ElementRegistry.getAllElements().isEmpty()) {
+		if(ElementRegistry.getAllElements().isEmpty())
 			ElementRegistry.init()
-		}
-		if(CompoundRegistry.compounds().isEmpty()) {
+
+		if(CompoundRegistry.compounds().isEmpty())
 			CompoundRegistry.init()
-		}
+
 		val parsedName = name.trim().lowercase(Locale.getDefault()).replace(" ", "_")
 		if(CompoundRegistry[parsedName] == null) {
 			val parsedComponents = components.map { x: ArrayList<Any?> ->
@@ -67,14 +66,35 @@ class Content : INamed {
 					(x[0] as String).lowercase(Locale.getDefault()).replace(" ", "_"), x[1] as Int
 				)
 			}
+			var realColor = color
+			if(color == null) {
+				var r = 0
+				var g = 0
+				var b = 0
+				parsedComponents.forEach {
+					val color = it.compound.color
+					r += color.red * it.quantity
+					g += color.green * it.quantity
+					b += color.blue * it.quantity
+				}
+				val size = parsedComponents.size.toDouble()
+				realColor = Color((r / size).roundToInt(), (g / size).roundToInt(), (b / size).roundToInt())
+			}
 			CompoundRegistry.addExternal(
 				meta,
 				parsedName,
-				Color(red.coerceIn(0, 255), green.coerceIn(0, 255), blue.coerceIn(0, 255)),
+				realColor,
 				parsedComponents
 			)
 		}
 	}
+
+	@MethodDescription(
+		type = MethodDescription.Type.ADDITION,
+		example = [Example("200, 'Advanced Hydrogen Molecule', 205, 205, 205, [['advanced_hydrogen', 1],['advanced_hydrogen', 1],['advanced_hydrogen', 1]]", commented = true)]
+	)
+	fun createCompound(meta: Int, name: String, red: Int, green: Int, blue: Int, components: ArrayList<ArrayList<Any?>>) =
+		createCompoundInternal(meta, name, Color(red.coerceIn(0, 255), green.coerceIn(0, 255), blue.coerceIn(0, 255)), components)
 
 	/**
 	 * In comparision to the method above, this method does not require the color of the compound to be set.
@@ -84,11 +104,8 @@ class Content : INamed {
 		type = MethodDescription.Type.ADDITION,
 		example = [Example("200, 'Advanced Hydrogen Molecule', [['advanced_hydrogen', 1],['advanced_hydrogen', 1],['advanced_hydrogen', 1]]", commented = true)]
 	)
-	fun createCompound(meta: Int, name: String, components: ArrayList<ArrayList<Any?>>) {
-		TODO("Implement the method here!")
-	}
+	fun createCompound(meta: Int, name: String, components: ArrayList<ArrayList<Any?>>) =
+		createCompoundInternal(meta, name, null, components)
 
-	override fun getAliases(): Collection<String?>? {
-		return listOf("content", "Content")
-	}
+	override fun getAliases() = listOf("content", "Content")
 }

6c9a3b188cc4d685ac9dcae61dcc114ee31db886
MasterEnderman <xxmr.endermanxx@gmail.com>
2025-04-04 00:41:18 +0200

    updated .gitignore and added groovyscript examples to repo (as the currently is no wiki for content generation), tweaked runConfig.json to work with the latest groovy version and use the actual alchemistry name space

---
+++ examples/preInit/alchemistry.groovy
@@ -0,0 +1,2 @@
+mods.alchemistry.content.createElement(200, 'Advanced Hydrogen', 'Ah', 255, 255, 255)
+mods.alchemistry.content.createCompound(200, 'Advanced Hydrogen Molecule', 205, 205, 205, [['advanced_hydrogen', 1],['advanced_hydrogen', 1],['advanced_hydrogen', 1]])

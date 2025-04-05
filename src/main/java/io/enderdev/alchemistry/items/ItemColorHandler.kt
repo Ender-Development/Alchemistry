2935639d6109163498943cb62f6de96d5b3c57fa
rozbrajaczpoziomow <roz@rozbrajacz.futbol>
2025-04-05 15:36:12 +0200

    add a little trolling that's less noticable/annoying (why even do it then? idk honestly)

--- src/main/java/io/enderdev/alchemistry/items/ItemColorHandler.kt
+++ src/main/java/io/enderdev/alchemistry/items/ItemColorHandler.kt
@@ -3,6 +3,7 @@ package io.enderdev.alchemistry.items
 import io.enderdev.alchemistry.chemistry.CompoundRegistry
 import io.enderdev.alchemistry.chemistry.ElementRegistry
 import net.minecraft.client.renderer.color.IItemColor
+import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
 import net.minecraftforge.fml.relauncher.Side
 import net.minecraftforge.fml.relauncher.SideOnly
@@ -22,17 +23,31 @@ class ItemColorHandler : IItemColor {
 		val item = stack.item
 		val meta = stack.metadata
 
-		return if(tintIndex != 0)
-			Color.WHITE.rgb
-		else if(april)
-			Color((meta * (stack.count ushr 2)).coerceAtMost(255), (meta + stack.count).coerceAtMost(255), meta ushr 1).rgb
+		val ret = if(tintIndex != 0)
+			Color.WHITE
 		else if(item is ItemElement)
-			if(meta > 118) ElementRegistry[meta]!!.color.rgb else Color.WHITE.rgb
+			if(meta > 118) ElementRegistry[meta]!!.color else Color.WHITE
 		else if(item is ItemElementIngot && ElementRegistry.keys().filter { it <= 118 }.contains(meta))
-			ElementRegistry[meta]!!.color.rgb
+			ElementRegistry[meta]!!.color
 		else if(item is ItemCompound && CompoundRegistry.keys().contains(meta))
-			CompoundRegistry[meta]!!.color.rgb
+			CompoundRegistry[meta]!!.color
 		else
-			Color.BLACK.rgb
+			Color.BLACK
+		return if(april)
+			aprilMultiplier(item, meta, ret)
+		else
+			ret.rgb
+	}
+
+	private fun aprilMultiplier(item: Item, meta: Int, original: Color): Int {
+		val offset = ((System.currentTimeMillis() shr 9) % 120L).toInt() - 60
+		return if(item is ItemElement && meta <= 118)
+			Color(195 + offset, 195 + offset, 195 + offset).rgb
+		else
+			Color(
+				(original.red + offset).coerceIn(0, 255),
+				(original.green + offset).coerceIn(0, 255),
+				(original.blue + offset).coerceIn(0, 255)
+			).rgb
 	}
 }
